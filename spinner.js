7639baedfe4707567a6f15b0d82685d4887044a5
Supermiu <supermiu@users.noreply.github.com>
2014-05-29 20:44:59 -0700

    dat function tho

--- spinner.js
+++ spinner.js
@@ -1,2 +1,10 @@
 //The js file for /spinner.html
 document.write("<h1>Welcome to Spinner Probability Simulator</h1>");
+var sections[];
+
+function add() {
+  var name = document.section.elements["sectionname"].value;
+  var percent = document.section.elements["percentage"].value;
+  document.write("<p>Added Section</p>" +name+ "<p>with percentage of</p>" +percent+ "<p>%</p>");
+}
+  
