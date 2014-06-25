e303b8d0e44c47572affc7cf65dccc3e55002399
Supermiu <supermiu@users.noreply.github.com>
2014-06-24 20:07:39 -0700

    javascript

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-a
+function show(id)
+{
+  var element = document.getElementById(id);
+  element.style.display = "none";
+}
+function hide(id)
+{
+  var element = document.getElementById(id);
+  element.style.display = "block";
+}
+  
