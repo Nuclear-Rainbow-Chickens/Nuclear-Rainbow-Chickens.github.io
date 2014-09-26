69f0694b2b4572f58bd025c5a8924659f80503db
supermiu <jeffersonlee2000@gmail.com>
2014-09-25 17:43:15 -0700

    looks better dont it

--- index_script.js
+++ index_script.js
@@ -1,6 +1,18 @@
 $("document").ready(function () {
-    $("#gamemenu").fadeOut(0);
-    $("#gamedrop, .gamelink").click(function() {
-        $("#gamemenu").fadeToggle(400);
+    
+    $("#gamedropmenu").css("width", addCSSpx($("#homebutton").css("width"), $("#gamedropbutton").css("width")));
+    $("#gamedropbutton, #gamedropmenu").mouseenter(function() {
+        $("#gamedropmenu").css("left", $("#width").css("width"));      
+        $("#gamedropmenu").css("top", addCSSpx($("#1stheight").css("height"), $("#2ndheight").css("height")));      
+        });
+    $("#gamedropbutton").mouseleave(function() {
+        $("#gamedropmenu").css("left", "-9999999999999px");   
         });
 });
+function addCSSpx(a,b) {
+    var anum = parseInt(a.replace("px", ""));
+    var bnum = parseInt(b.replace("px",""));
+    var newnum = anum+bnum;
+    var abstr = newnum+"px";
+    return abstr;
+}
