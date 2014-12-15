fca56435f77079d621ae97bfc04bb8f9bd6a6a29
supermiu <jeffersonlee2000@gmail.com>
2014-12-14 21:20:56 -0800

    location script

---
+++ locate/locate.js
@@ -0,0 +1,67 @@
+var lat = 0;
+var long = 0;
+function getCoords() {	
+	if(navigator.geolocation) {
+		htmlgeo();
+	}
+	else {
+		ipCheck();	
+	}
+}
+
+function detectmob() { 
+ if( navigator.userAgent.match(/Android/i)
+ || navigator.userAgent.match(/webOS/i)
+ || navigator.userAgent.match(/iPhone/i)
+ || navigator.userAgent.match(/iPad/i)
+ || navigator.userAgent.match(/iPod/i)
+ || navigator.userAgent.match(/BlackBerry/i)
+ || navigator.userAgent.match(/Windows Phone/i)
+ ){
+    return true;
+  }
+ else {
+    return false;
+  }
+}
+			
+function ipCheck() {
+	jQuery.getJSON("http://ip-api.com/json", function (data) {
+		if(data.status != "fail") {
+			lat = data.lat;
+			long = data.lon;
+		}
+		else {
+			alert("ERROR ALL FALLBACKS REACHED AND FAILED!");	
+		}
+		printResults(false);	
+	});
+}
+
+function htmlgeo() {
+	if(detectmob()) {
+		navigator.geolocation.watchPosition(positiveCallback, ipCheck, {enableHighAccuracy: true});
+	}
+	else {
+		navigator.geolocation.getCurrentPosition(positiveCallback, ipCheck, {enableHighAccuracy: true});
+	}
+}
+
+function positiveCallback(pos) {
+	lat = pos.coords.latitude;
+	long = pos.coords.longitude;
+	printResults();
+}
+
+function printResults(a) {
+	document.getElementById("txt").innerHTML = "Latitude: "+lat + "\n" + "Longitude: " + long;
+	if(detectmob() && a) {
+		document.getElementById("mobile").innerHTML = "YOU\'RE ON MOBILE, RUN AROUND IN CIRCLES AND SEE WHAT HAPPENS!" ;	
+	}
+	else if(detectmob() && !a) {
+		document.getElementById("mobile").innerHTML = "ON MOBILE BUT DENIED ACCESS TO GPS" ;
+	}
+	else {
+		document.getElementById("mobile").innerHTML = "DESKTOP/LAPTOP";
+	}
+}
